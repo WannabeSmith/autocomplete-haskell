bf7d06aa586126d9db7b937c15663adc5d94855c
Ian Waudby-Smith <5490890+WannabeSmith@users.noreply.github.com>
2021-09-19 20:07:08 -0400

    Add Unity as git repo

--- c_library/run_tests.sh
+++ c_library/run_tests.sh
@@ -11,7 +11,7 @@ stack build
 stack ghc -- -c -O src/CTries.hs -stubdir c_library/dependencies
 
 # Compile the C functions and create tests to be run
-stack ghc -- --make -no-hs-main -optc-O -Ic_library/dependencies -Ic_library/src c_library/test/test_tries.c c_library/dependencies/unity.c c_library/src/tries.c src/CTries -o c_library/test/test_tries
+stack ghc -- --make -no-hs-main -optc-O -Ic_library/dependencies -Ic_library/src c_library/test/test_tries.c c_library/dependencies/Unity/src/unity.c c_library/src/tries.c src/CTries -o c_library/test/test_tries
 
 # run unit tests
 c_library/test/test_tries
--- c_library/test/test_tries.c
+++ c_library/test/test_tries.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include "tries.h"
-#include "unity.h"
+#include "Unity/src/unity.h"
 
 void setUp(void) {
 }
